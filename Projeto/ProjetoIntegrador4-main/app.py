74bc32571b11d695eecf36f8ed99fff63818f17f
alineelaine <121210728+alineelaine@users.noreply.github.com>
2024-05-23 21:43:21 -0300

    backend login

---
+++ Projeto/ProjetoIntegrador4-main/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_mysqldb import MySQL
+import hashlib
+
+app = Flask(__name__)
+app.config['MYSQL_USER'] = 'aline'  
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_PASSWORD'] = 'Newell01@'  
+app.config['MYSQL_DB'] = 'EducaAnalytics'  
+
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        id_usuario = request.form['username']
+        senha = request.form['password']
+        senha_hash = hashlib.sha256(senha.encode()).hexdigest()
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM tLogins WHERE ID_Usuario=%s AND Senha=%s", (id_usuario, senha_hash))
+        usuario = cur.fetchone()
+        cur.close()
+        if usuario:
+            return redirect(url_for('success'))
+        else:
+            return 'Falha no login!'
+    return render_template('login.html')
+
+@app.route('/success')
+def success():
+    return 'Deu certo!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
