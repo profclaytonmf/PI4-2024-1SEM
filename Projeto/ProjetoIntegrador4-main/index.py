f74c8fc14bfc797f004a03ad3e5d6c4678095104
alineelaine <121210728+alineelaine@users.noreply.github.com>
2024-05-30 16:42:21 -0300

    index

--- Projeto/ProjetoIntegrador4-main/index.py
+++ Projeto/ProjetoIntegrador4-main/index.py
@@ -5,24 +5,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    alcadas = pd.read_excel('controle.xlsx', sheet_name='alcadas').to_html(index=False)
-    email = pd.read_excel('controle.xlsx', sheet_name='email').to_html(index=False)
-    comite = pd.read_excel('controle.xlsx', sheet_name='comite').to_html(index=False)
-    ficticios = pd.read_excel('controle.xlsx', sheet_name='ficticios').to_html(index=False)
-    ajustes = pd.read_excel('controle.xlsx', sheet_name='ajustes').to_html(index=False)
-
-    return render_template('index.html', alcadas=alcadas, email=email, comite=comite, ficticios=ficticios, ajustes=ajustes)
-@app.route('/pagina1')
-def pagina1():
-    return render_template('pagina1.html')
-
-@app.route('/pagina2')
-def pagina2():
-    return render_template('pagina2.html')
-
-@app.route('/pagina3')
-def pagina3():
-    return render_template('pagina3.html')
+    return render_template('indexAlunos.html')
+@app.route('/AlunoAutonomia')
+def AlunoAutonomia():
+    return render_template('formsAlunosAutonomia.html')
+@app.route('/AlunoEnsino')
+def AlunoEnsino():
+    return render_template('formsAlunosQualidadeEnsino.html')
+@app.route('/AlunoClima')
+def AlunoClima():
+    return render_template('formsAlunosClimaEscolar.html')
+@app.route('/AlunoInfra')
+def AlunosInfra():
+    return render_template('formsAlunosInfraestrutura.html')
+@app.route('/AlunoGestao')
+def AlunosGestao():
+    return render_template('formsAlunosGestao.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
