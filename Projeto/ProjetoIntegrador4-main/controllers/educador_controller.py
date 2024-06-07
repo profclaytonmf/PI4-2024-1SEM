67f75983f6068fb90f694ad791b1d0311b73482c
Thiago <thiago.bafi@outlook.com>
2024-06-06 23:56:48 -0300

    criar formEducadorGestao

--- Projeto/ProjetoIntegrador4-main/controllers/educador_controller.py
+++ Projeto/ProjetoIntegrador4-main/controllers/educador_controller.py
@@ -24,7 +24,7 @@ def EducadorInfra():
 
 @educador_blueprint.route('/EducadorGestao')
 def EducadorGestao():
-    return render_template('formsFuncionariosGestao.html')
+    return render_template('formsEducadoresGestao.html')
 
 @educador_blueprint.route('/EducadorParticipacao')
 def EducadorParticipacao():
